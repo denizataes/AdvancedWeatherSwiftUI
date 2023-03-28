4e9cbd1cf7140bf8a97fbe71fb96218459b943e4
Deniz Ata EŞ <85442526+denizataes@users.noreply.github.com>
2023-03-28 20:47:58 +0300

    Initial Commit

---
+++ AdvancedWeatherSwiftUI/AdvancedWeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedWeatherSwiftUIApp.swift
+//  AdvancedWeatherSwiftUI
+//
+//  Created by Deniz Ata Eş on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedWeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
