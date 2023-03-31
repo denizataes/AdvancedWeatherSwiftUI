3dc3a099d64f1fdf7e97c9611877b62b8f44b443
Deniz Ata EŞ <85442526+denizataes@users.noreply.github.com>
2023-03-31 22:04:20 +0300

    added some animations to bottom sheet

--- AdvancedWeatherSwiftUI/Views/Main/HomeView.swift
+++ AdvancedWeatherSwiftUI/Views/Main/HomeView.swift
@@ -15,72 +15,98 @@ enum BottomSheetPosition: CGFloat, CaseIterable{
 
 struct HomeView: View {
     @State var bottomSheetPosition: BottomSheetPosition = .middle
+    @State var bottomSheetTranslation: CGFloat = BottomSheetPosition.middle.rawValue
+    @State var hasDragged: Bool = false
+    var bottomSheetTranslationProrated: CGFloat {
+        (bottomSheetTranslation - BottomSheetPosition.middle.rawValue) / (BottomSheetPosition.top.rawValue - BottomSheetPosition.middle.rawValue)
+    }
     var body: some View {
         NavigationView {
-            ZStack{
-                // MARK: Background Color
-                Color.background
-                    .ignoresSafeArea()
+            GeometryReader { geometry in
+                let screenHeight = geometry.size.height + geometry.safeAreaInsets.top + geometry.safeAreaInsets.bottom
                 
+                let imageOffset = screenHeight + 36
                 
-                // MARK: Backround Image
-                
-                Image("Background")
-                    .resizable()
-                    .ignoresSafeArea()
-                
-                
-                // MARK: House Image
-                
-                Image("House")
-                    .frame(maxHeight: .infinity, alignment: .top)
-                    .padding(.top, 257)
-                
-                VStack(spacing: -10){
-                    Text("Ankara")
-                        .font(.largeTitle)
+                ZStack{
+                    // MARK: Background Color
+                    Color.background
+                        .ignoresSafeArea()
                     
-                    VStack {
+                    
+                    // MARK: Backround Image
+                    
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    // MARK: House Image
+                    
+                    Image("House")
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .padding(.top, 257)
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    VStack(spacing: -10 * (1 - bottomSheetTranslationProrated)){
+                        Text("Ankara")
+                            .font(.largeTitle)
+                        
+                        VStack {
 
-                    Text(attributedString)
+                        Text(attributedString)
+                            
+                        Text("H:24°   L:18°")
+                                .font(.title3.weight(.semibold))
+                                .opacity(1 - bottomSheetTranslationProrated)
+                            
+                        }
                         
-                    Text("H:24°   L:18°")
-                            .font(.title3.weight(.semibold))
-                        
+                        Spacer()
                     }
+                    .padding(.top, 51)
+                    .offset(y: -bottomSheetTranslationProrated * 46)
                     
-                    Spacer()
-                }
-                .padding(.top, 51)
-                
-                // MARK: Bottom Sheet
-                BottomSheetView(position: $bottomSheetPosition) {
-                    
-                } content: {
-                    ForecastView()
-                }
+                    // MARK: Bottom Sheet
+                    BottomSheetView(position: $bottomSheetPosition) {
+                        
+                    } content: {
+                        ForecastView(bottomSheetTranslationProrated: bottomSheetTranslationProrated)
+                    }
+                    .onBottomSheetDrag { translation in
+                        bottomSheetTranslation = translation / screenHeight
+                        withAnimation(.easeInOut){
+                            if bottomSheetPosition == BottomSheetPosition.top{
+                                hasDragged = true
+                            }else{
+                                hasDragged = false
+                            }
+                        }
+                       
+                    }
 
-                
-                // MARK: Tabbar
-                TabBar(action: {
-                    bottomSheetPosition = .top
-                })
+                    
+                    // MARK: Tabbar
+                    TabBar(action: {
+                        bottomSheetPosition = .top
+                    })
+                    .offset(y: bottomSheetTranslationProrated * 115)
+                }
+                .navigationBarHidden(true)
             }
-            .navigationBarHidden(true)
         }
     }
     
     private var attributedString: AttributedString{
-        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        var string = AttributedString("19°" + (hasDragged ? " | " : "\n ") + "Mostly Clear")
         
         if let temp = string.range(of: "19°"){
-            string[temp].font = .system(size: 96, weight: .thin)
-            string[temp].foregroundColor = .primary
+            string[temp].font = .system(size: (96 - (bottomSheetTranslationProrated * (96 - 20))), weight: hasDragged ? .semibold : .thin)
+            string[temp].foregroundColor = hasDragged ? .secondary : .primary
         }
         
         if let pipe = string.range(of: " | "){
             string[pipe].font = .title3.weight(.semibold)
-            string[pipe].foregroundColor = .secondary
+            string[pipe].foregroundColor = .secondary.opacity(bottomSheetTranslationProrated)
         }
         
         if let weather = string.range(of: "Mostly Clear"){
